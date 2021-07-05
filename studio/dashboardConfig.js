4fccee258736626629581d60389d07b590b6492c
Bryan Reed <83431578+B-c0des@users.noreply.github.com>
2021-07-04 23:55:10 -0700

    Update dashboardConfig.js

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -39,7 +39,7 @@ export default {
             value: 'https://github.com/B-c0des/sanity-nextjs-landing-pages',
             category: 'Code'
           },
-          {title: 'Frontend', value: 'https://brians-portfolio.netlify.app', category: 'apps'}
+          {title: 'Frontend', value: 'https://brian-reed.netlify.app/', category: 'apps'}
         ]
       }
     },
